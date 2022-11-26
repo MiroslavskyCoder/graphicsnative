8a771b53b3ff8bba04f7c1e6b62fc1f0596086cd
MiroslavskyCoder <alinowski2016@yandex.ru>
2022-11-26 21:24:02 +0300

    fix: rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,7 @@
 import jsonPlugin from '@rollup/plugin-json';
 import typescriptPlugin from 'rollup-plugin-typescript2';
 import babel from 'rollup-plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser'; 
+import resolve from '@rollup/plugin-node-resolve'; 
 import sourceMaps from 'rollup-plugin-sourcemaps';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
@@ -18,20 +17,20 @@ const env = process.env.NODE_ENV || 'prod';
 const coreModules = builtinModules.filter(name => !/(^_|\/)/.test(name));
 const cacheRoot = pathJoin(tmpdir(), '.rpt2_cache');
 
-function makePathOutput(target = "cjs") {
+function makePathOutput() {
     return [
         {
-            file: pathJoin("lib", target, 'index.js'),
+            file: pathJoin("lib", 'index.js'),
             format: 'cjs',
             exports: 'named'
         },
         {
-            file: pathJoin("lib", target, 'index.es.mjs'),
+            file: pathJoin("lib", 'index.es.mjs'),
             format: "es",
             sourcemap: true
         },
         {
-            file: pathJoin("lib", target, 'index.mjs'),
+            file: pathJoin("lib", 'index.mjs'),
             format: "esm"
         } 
     ];
@@ -50,8 +49,7 @@ const pluginsCommon = [
     replace({
         exclude: 'node_modules/**',
         'process.env.NODE_ENV': JSON.stringify(env),
-    }),
-    terser()
+    })
 ]
 
 const external = [
@@ -60,7 +58,7 @@ const external = [
     ...coreModules
 ];
  
-const rollupES5 = {
+const commonRollupConfig = {
     input,
     plugins: [ 
         ...pluginsCommon,
@@ -68,7 +66,7 @@ const rollupES5 = {
         typescriptPlugin({
             cacheRoot, 
             useTsconfigDeclarationDir: false, 
-            tsconfig: "./tsconfig-esm5.json",
+            tsconfig: "./tsconfig.json",
             tsconfigOverride: {
                 outDir: "lib",
                 rootDir: "src",
@@ -79,148 +77,11 @@ const rollupES5 = {
     ],
     external,
     output: [
-        ...makePathOutput("es5")
+        ...makePathOutput()
     ]
 }
-const rollupES6 = {
-    input,
-    plugins: [ 
-        ...pluginsCommon,
-        uglify(),
-        typescriptPlugin({
-            cacheRoot, 
-            useTsconfigDeclarationDir: false, 
-            tsconfig: "./tsconfig-esm6.json",
-            tsconfigOverride: {
-                outDir: "lib",
-                rootDir: "src",
-                include: ["src"]
-            },
-            clean: true
-        }) 
-    ],
-    external,
-    output: [
-        ...makePathOutput("es6")
-    ]
-}
-const rollupES2018 = {
-    input,
-    plugins: [ 
-        ...pluginsCommon,
-        uglify(),
-        typescriptPlugin({
-            cacheRoot, 
-            useTsconfigDeclarationDir: false, 
-            tsconfig: "./tsconfig-es2018.json",
-            tsconfigOverride: {
-                outDir: "lib",
-                rootDir: "src",
-                include: ["src"]
-            },
-            clean: true
-        }) 
-    ],
-    external,
-    output: [
-        ...makePathOutput("es2018")
-    ]
-}
-const rollupES2019 = {
-    input,
-    plugins: [ 
-        ...pluginsCommon,
-        uglify(),
-        typescriptPlugin({
-            cacheRoot, 
-            useTsconfigDeclarationDir: false, 
-            tsconfig: "./tsconfig-es2019.json",
-            tsconfigOverride: {
-                outDir: "lib",
-                rootDir: "src",
-                include: ["src"]
-            },
-            clean: true
-        }) 
-    ],
-    external,
-    output: [
-        ...makePathOutput("es2019")
-    ]
-}
-const rollupES2020 = {
-    input,
-    plugins: [ 
-        ...pluginsCommon,
-        uglify(),
-        typescriptPlugin({
-            cacheRoot, 
-            useTsconfigDeclarationDir: false, 
-            tsconfig: "./tsconfig-es2020.json",
-            tsconfigOverride: {
-                outDir: "lib",
-                rootDir: "src",
-                include: ["src"]
-            },
-            clean: true
-        }) 
-    ],
-    external,
-    output: [
-        ...makePathOutput("es2020")
-    ]
-}
-const rollupES2021 = {
-    input,
-    plugins: [ 
-        ...pluginsCommon,
-        uglify(),
-        typescriptPlugin({
-            cacheRoot, 
-            useTsconfigDeclarationDir: false, 
-            tsconfig: "./tsconfig-es2021.json",
-            tsconfigOverride: {
-                outDir: "lib",
-                rootDir: "src",
-                include: ["src"]
-            },
-            clean: true
-        }) 
-    ],
-    external,
-    output: [
-        ...makePathOutput("es2021")
-    ]
-}
-const rollupES2022 = {
-    input,
-    plugins: [ 
-        ...pluginsCommon,
-        uglify(),
-        typescriptPlugin({
-            cacheRoot, 
-            useTsconfigDeclarationDir: false, 
-            tsconfig: "./tsconfig-es2022.json",
-            tsconfigOverride: {
-                outDir: "lib",
-                rootDir: "src",
-                include: ["src"]
-            },
-            clean: true
-        }) 
-    ],
-    external,
-    output: [
-        ...makePathOutput("es2022")
-    ]
-}
-
+ 
+ 
 export default [
-    rollupES5,
-    rollupES6,
-    rollupES2018,
-    rollupES2019,
-    rollupES2020,
-    rollupES2021,
-    rollupES2022
+    commonRollupConfig
 ];
