8c59e331bc854648f16158654109201f7ac156ae
MiroslavskyCoder <alinowski2016@yandex.ru>
2022-11-28 20:00:30 +0300

    fix: copyLinuxImageMagick.js

--- scripts/copyLinuxImageMagick.js
+++ scripts/copyLinuxImageMagick.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.copyFileSync("deps_image_magick/MagickWand/.libs/libMagickWand-7.Q16HDRI.so", path.join(process.argv[2], "libMagickWand-7.Q16HDRI.so"));
-fs.copyFileSync("deps_image_magick/MagickWand/.libs/libMagickCore-7.Q16HDRI.so", path.join(process.argv[2], "libMagickCore-7.Q16HDRI.so"));
-fs.copyFileSync("deps_image_magick/MagickWand/.libs/libMagick++-7.Q16HDRI.so", path.join(process.argv[2], "libMagick++-7.Q16HDRI.so"));
+fs.copyFileSync(path.resolve("deps_image_magick/MagickWand/.libs/libMagickWand-7.Q16HDRI.so.10.0.0"), path.join(process.argv[2], "libMagickWand-7.Q16HDRI.so"));
+fs.copyFileSync(path.resolve("deps_image_magick/MagickCore/.libs/libMagickCore-7.Q16HDRI.so.10.0.0"), path.join(process.argv[2], "libMagickCore-7.Q16HDRI.so"));
+fs.copyFileSync(path.resolve("deps_image_magick/Magick++/lib/.libs/libMagick++-7.Q16HDRI.so.5.0.0"), path.join(process.argv[2], "libMagick++-7.Q16HDRI.so"));
