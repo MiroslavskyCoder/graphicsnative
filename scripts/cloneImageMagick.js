8119d922d9662cbe4d864514a7283a9d82f4f30b
MiroslavskyCoder <alinowski2016@yandex.ru>
2022-11-28 21:31:39 +0300

    fix: clone ImageMagick

--- scripts/cloneImageMagick.js
+++ scripts/cloneImageMagick.js
@@ -3,12 +3,12 @@ const fs = require("fs");
 const os = require("os");
 
 if (os.platform() == "linux") {
-    if (
-        fs.statSync("deps_image_magick").isDirectory()
-    ) {
-        child.execSync("rm -r deps_image_magick");
+    if (fs.existsSync("deps_image_magick")) {
+        if (!fs.statSync("deps_image_magick").isDirectory()) {
+            child.execSync("rm deps_image_magick");
+        }
+        else child.execSync("rm -r deps_image_magick");
     }
-    
     child.execSync("git clone https://github.com/ImageMagick/ImageMagick.git deps_image_magick");
     child.execSync("cd deps_image_magick && bash ./configure --with-modules");
     child.execSync("cd deps_image_magick && make -j 4");
